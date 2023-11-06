dcd912b97a6c9904846dbc45da2263bf99ee6a96
tcotidiane33 <tcotidiane33@gmail.com>
2023-11-06 16:00:03 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+// telegram token
+ var token = '6630470146:AAHKxRgt0-dkfeV4vCaGj3afshe7aONOkgI'
+// firebase config
+var config = {
+    apiKey: "AIzaSyA9bykZg5YFBQJlGSIi8i_RXV7UNonfI8U",
+    authDomain: "novohub.firebaseapp.com",
+    projectId: "novohub",
+    storageBucket: "novohub.appspot.com",
+    messagingSenderId: "486940016289",
+    appId: "1:486940016289:web:9681ba11f18f32070b3ae0",
+    measurementId: "G-D30MBTXCL7"
+};
+
+exports.token = token;
+exports.firebase_config = config;
